36124c2925cd102b74425f4c0913aeda59489495
Prosper <nkonyeprospero@gmail.com>
2024-05-15 01:19:33 +0100

    Versioning untracked files

--- components/eccormerce/frontpage/TodayDeals.js
+++ components/eccormerce/frontpage/TodayDeals.js
@@ -4,7 +4,7 @@ import ShowCaseTwo from "../showcase/showCaseTwo";
 export default function TodayDeals({ todaydeals }) {
   return (
     <div className="w-full flex overflow-x-auto lg:overflow-hidden bg-[#fff] rounded-lg ">
-      <div className="p-2 flex justify-around  w-[100%] ">
+      <div className="p-2 flex justify-around w-[100%]">
         <ShowCaseTwo data={todaydeals[0]} />
         <ShowCaseTwo data={todaydeals[1]} />
         <ShowCaseTwo data={todaydeals[2]} />
